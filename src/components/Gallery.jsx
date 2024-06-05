58f63d17ab52c9e0900053d7240fd7154d6a3ba8
Ekatmoz <ekatmoz@gmail.com>
2024-06-05 12:31:36 +0200

    added pictures to gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -14,6 +14,30 @@ return (
       <Row className="portfolio-isotope">
          <Row className="gy-4 portfolio-container">
             <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+               <div className="portfolio-content h-100">
+                  <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582298/received_1807985649709637_k7dgqd.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Ремонтные работы</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+               <div className="portfolio-content h-100">
+                  <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582298/received_2213934898940614_eczbhx.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Ремонтные работы</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+               <div className="portfolio-content h-100">
+                  <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582298/received_1730673840795896_yw6ji8.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Ремонтные работы</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
             <div className="portfolio-content h-100">
                <img src="/images/remonti.jpg" className="img-fluid" alt="" />
                <div className="portfolio-info">
@@ -53,9 +77,58 @@ return (
                </div>
             </div>
             </Col>
+            
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582310/review2_cczdlu.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Укладка плитки</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582303/review3_gbp0jp.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Укладка плитки</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582298/received_1630665684337060_i1gy7j.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Укладка плитки</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582294/FB_IMG_1695627719545_r3ihwn.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Укладка плитки</h4>
+               </div>
+            </div>
+            </Col>
             <Col lg={4} md={6} className="portfolio-item filter-remodeling">
             <div className="portfolio-content h-100">
                <img src="/images/houses.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Фасад домов</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582297/received_1142206603596052_jdh9hc.jpg" className="img-fluid" alt="" />
+               <div className="portfolio-info">
+                  <h4>Фасад домов</h4>
+               </div>
+            </div>
+            </Col>
+            <Col lg={4} md={6} className="portfolio-item filter-remodeling">
+            <div className="portfolio-content h-100">
+               <img src="https://res.cloudinary.com/dtj7rhgwl/image/upload/v1717582264/20240410_132455_xxuxwo.jpg" className="img-fluid" alt="" />
                <div className="portfolio-info">
                   <h4>Фасад домов</h4>
                </div>
